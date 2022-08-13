4a72fc052f51deccc2f43493a8911505208f0cb8
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-08-14 00:26:04 +0300

    Добавлен класс для ячейки

---
+++ Currency Converter/ViewControllers/Views/CurrencyTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CurrencyTableViewCell.swift
+//  Currency Converter
+//
+//  Created by Алексей on 14.08.2022.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
